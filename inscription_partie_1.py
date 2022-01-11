6554df5b91bc9664e2a8c698b77a1d63277f3506
johan-valero <valero_johan@hotmail.fr>
2022-01-11 16:19:29 +0100

    add fonction.py

--- inscription_partie_1.py
+++ inscription_partie_1.py
@@ -1,28 +1,6 @@
 # Partie 1 : inscription.py V1 (prérequis : input, parcours de liste/dict, opération math, opération littéraires)
 
-#Catégorie d'un inscrit vis à vis de son age
-
-def categories(annee):
-    age = 2022 - int(annee)
-    if 6 <= age < 12:
-        return "poussin"
-    elif 12 <= age < 18:
-        return "cadet"
-    elif 18 <= age < 24:
-        return "junior"
-    elif 24 <= age < 30:
-        return "semipro"
-    elif 30 <= age <= 40:
-        return "pro"
-    else:
-        print("non-admis")
-
-#adresse mail automatique
-
-def email(nom,prenom):
-    mail = str(prenom[0]+"."+nom+"@baton-rouge.fr")
-    return mail
-
+from Fonction import email, categories
 
 nom = input("Veuillez renseigner le nom ? \n")
 prenom = input("Veuillez renseigner le prénom ? \n")
--- inscription_partie_2.py
+++ inscription_partie_2.py
@@ -1,27 +1,6 @@
 # Partie 2 : inscription.py V2 (prérequis : logique conditionnelle, opérations logiques, exceptions)
 
-#Catégorie d'un inscrit vis à vis de son age
-
-def categories(annee):
-    age = 2022 - int(annee)
-    if 6 <= age < 12:
-        return "Poussin"
-    elif 12 <= age < 18:
-        return "Cadet"
-    elif 18 <= age < 24:
-        return "Junior"
-    elif 24 <= age < 30:
-        return "Semi-pro"
-    elif 30 <= age <= 40:
-        return "Pro"
-    else:
-        print(" Non-admis")
-
-#adresse mail automatique
-
-def email(nom,prenom):
-    mail = str(prenom[0]+"."+nom+"@baton-rouge.fr")
-    return mail
+from Fonction import email, categories
 
 # Gestioin des erreurs : sur le nombre d'inscrit
 
